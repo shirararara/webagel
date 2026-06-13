d0ddd65c8c14fe2e789428d086977c9a433aa9ee
shirararara <shestakbogdan123@gmail.com>
2026-06-13 23:39:09 +0300

    Refactor Supabase client initialization

--- js/supabase.js
+++ js/supabase.js
@@ -2,7 +2,7 @@ import { createClient }
 from "https://esm.sh/@supabase/supabase-js";
 
 const SUPABASE_URL =
-"https://gbdboswbigpwidwocycw.supabase.co/rest/v1/";
+"https://gbdboswbigpwidwocycw.supabase.co";
 
 const SUPABASE_KEY =
 "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdiZGJvc3diaWdwd2lkd29jeWN3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODEzNzUyMzMsImV4cCI6MjA5Njk1MTIzM30.qKcBYWtCa73bXtmra9lfibfoJ-NFpruBvLuTw62Q-n4";
